d5e378ba9045057d60b06f80ae2ee648c84580f8
Thomas Watson Steen <w@tson.dk>
2016-11-24 10:28:17 +0100

    code cleanup

--- index.js
+++ index.js
@@ -22,18 +22,20 @@ module.exports = function (opts) {
       var prefix = opts.prefix
       var normalizedLevel
 
+      if (opts.stderr) {
+        normalizedLevel = 'error'
+      } else {
+        switch (level) {
+          case 'trace': normalizedLevel = 'info'; break
+          case 'debug': normalizedLevel = 'info'; break
+          case 'fatal': normalizedLevel = 'error'; break
+          default: normalizedLevel = level
+        }
+      }
+
       if (prefix) {
         if (typeof prefix === 'function') prefix = prefix()
         arguments[0] = util.format(prefix, arguments[0])
-      }
-
-      if (opts.stderr) return console.error.apply(console, arguments)
-
-      switch (level) {
-        case 'trace': normalizedLevel = 'info'; break
-        case 'debug': normalizedLevel = 'info'; break
-        case 'fatal': normalizedLevel = 'error'; break
-        default: normalizedLevel = level
       }
 
       console[normalizedLevel](util.format.apply(util, arguments))
